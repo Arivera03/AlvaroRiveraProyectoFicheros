cc8074f543ae333111102f344692f50977b952d1
alvrivviv <alvaroriveravivero@gmail.com>
2024-12-03 12:25:37 +0100

    avanzado(?)

--- src/main/java/com/alvaro/main.java
+++ src/main/java/com/alvaro/main.java
@@ -1,7 +1,18 @@
 package com.alvaro;
 
+import java.util.Scanner;
+
 public class main {
     public static void main(String[] args) {
-        
+        try {
+            Generador generador = new Generador();
+
+            generador.generateSite();
+
+            System.out.println("¡Sitio web generado con éxito!");
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println("Error al generar el sitio web: " + e.getMessage());
+        }
     }
 }
